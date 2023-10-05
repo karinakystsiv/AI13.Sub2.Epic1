16ebd765a950dc074c83cafda3a69caa4eb5e945
OlegBandrivskiy26 <bandroleg26@gmail.com>
2023-10-05 21:31:01 +0300

    change file main1

--- main1.cpp
+++ main1.cpp
@@ -4,7 +4,8 @@
 int main()
 {
     std::cout<<"Karina Kystsiv" << std::endl;
-    std::cout<<"Novosad Olena";
+    std::cout<<"Olena Novosad"  << std::endl;
+    std::cout<<"Oleg Bandrivskiy" << std::endl;
     return 0;
  
 }
