9c22a9dc937ffe20ab67fa824922538c071f0934
Olena Novosad <olena.novosad.shi.2023@lpnu.ua>
2023-10-05 21:08:41 +0300

    modified file main1_added name

--- main1.cpp
+++ main1.cpp
@@ -3,13 +3,8 @@
 
 int main()
 {
-<<<<<<< HEAD
     std::cout<<"Karina Kystsiv" << std::endl;
     std::cout<<"Novosad Olena";
-=======
-    std::cout<<"Karina Kystsiv";
-    std::cout<<"Oleg Bandrivskiy";
->>>>>>> fc4a774794584bc35d389cdb00540b2d22900b07
     return 0;
  
 }
