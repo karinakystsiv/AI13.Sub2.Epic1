eb51cfb358a47d95bb1bffbc5c2fc09ab0c21a01
Artem Laskavyi <Artem.Laskavyi.ShI.2023@lpnu.ua>
2023-10-05 22:46:02 +0300

    modified main1 - added my name

--- main1.cpp
+++ main1.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
+
 int main()
 {
     std::cout<<"Karina Kystsiv" << std::endl;
+    std::cout<<"Olena Novosad"  << std::endl;
     std::cout<<"Oleg Bandrivskiy" << std::endl;
-    std::cout << "Laskavyi Artem" << std::endl;
-
+    std::cout<<"Artem Stepanyuk" << std::endl;
+    std::cout<<"Artem Laskavyi" << std::endl;
+    
     return 0;
-
+ 
 }
